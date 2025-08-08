cc70f0a8db99005e3447241502c6ddb3c5121f9e
RamAvni <ramavni@hilma.tech>
2025-08-08 10:23:28 +0300

    changed a bit of the neotree config, to match what i'm used to from lazyvim

--- nvim/lua/custom/neo-tree.lua
+++ nvim/lua/custom/neo-tree.lua
@@ -1,6 +1,3 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -11,13 +8,15 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>e'] = 'close_window',
+          ['l'] = 'open',
+          ['h'] = 'close_node',
         },
       },
     },
